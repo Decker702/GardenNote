9eae3036e22fbfff8261f3f31f9b7f220882318b
LDecker <Decker702@yahoo.com>
2017-05-09 23:50:04 -0400

    Create Note List Item

---
+++ GardenNote.Web/Controllers/NoteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GardenNote.Models;
+
+namespace GardenNote.Web.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            //temp data
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+    }
+}
